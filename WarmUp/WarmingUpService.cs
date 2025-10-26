9a9bfa9f29e87514aa96a56bb093f2d63aa134e9
mohamed <mohamed.almohsen@outlook.com>
2025-10-26 23:42:06 +0100

    Warmed up

---
+++ WarmUp/WarmingUpService.cs
@@ -0,0 +1,60 @@
+﻿using System.Text;
+
+namespace WarmUp;
+
+public class WarmingUpService
+{
+    public static bool IsPowerOfTwo(int bookId)
+    {
+        if (bookId < 1)
+        {
+            return false;
+        }
+        else if (bookId == 1)
+        {
+            return true;
+        }
+        else if (bookId % 2 == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public static string ReverseString(string title)
+    {
+        if (string.IsNullOrWhiteSpace(title))
+        {
+            return title;
+        }
+        else
+        {
+            return new string(title.ToCharArray().Reverse().ToArray());
+        }
+    }
+
+    public static string ReplicateString(string title, int numberOfReplicas)
+    {
+        if (numberOfReplicas == 0)
+        {
+            return "";
+        }
+        else if (numberOfReplicas == 1)
+        {
+            return title;
+        }
+        else
+        {
+            var sb = new StringBuilder(title.Length * numberOfReplicas);
+            
+            for (int i = 0; i < numberOfReplicas; i++)
+            {
+                sb.Append(title);
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ WarmUpTest/WarmingUpServiceTests.cs
@@ -0,0 +1,51 @@
+﻿using WarmUp;
+using Xunit.Abstractions;
+
+namespace WarmUpTest;
+
+public class WarmingUpServiceTests
+{
+    private readonly ITestOutputHelper _testOutputHelper;
+
+    public WarmingUpServiceTests(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
+
+    [Fact]
+    public void TestIsPowerOfTwo()
+    {
+        var bookId = -1;
+        var isPowerOfTwo = WarmingUpService.IsPowerOfTwo(bookId);
+        _testOutputHelper.WriteLine($"{bookId} is power of two? {isPowerOfTwo.ToString()}");
+    }
+    
+    [Fact]
+    public void TestStringReverser()
+    {
+        var sampleString = "Hello";
+        var reversedString = WarmingUpService.ReverseString(sampleString);
+        _testOutputHelper.WriteLine($"{sampleString} reversed is {reversedString}");
+    }
+
+    [Fact]
+    public void TestStringReplicator()
+    {
+        var sampleString = "Hello";
+        var numberOfReplicas = 2;
+        var replicatedString = WarmingUpService.ReplicateString(sampleString, numberOfReplicas);
+        _testOutputHelper.WriteLine($"{sampleString} replicated {numberOfReplicas} times is {replicatedString}");
+    }
+
+    [Fact]
+    public void OddNumbersWriter()
+    {
+        for (int i = 0; i < 101; i++)
+        {
+            if (i % 2 != 0)
+            {
+                _testOutputHelper.WriteLine($"{i} is odd number");
+            }
+        }
+    }
+}
